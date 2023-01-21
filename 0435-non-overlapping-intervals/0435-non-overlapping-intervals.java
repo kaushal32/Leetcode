11a704cb440c5f9b9d6ba483272d893daee71b22
Kaushal Kumar <90971842+kaushal32@users.noreply.github.com>
2023-01-21 22:50:51 +0530

    Time: 10 ms (99.56%), Space: 95.3 MB (97.71%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,27 +1,37 @@
 class Solution {
-    public int eraseOverlapIntervals(int[][] intervals) {
-        int n = intervals.length;
-        
-        if(n == 0) {
-            return 0;
-        }
-        
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-        
-        int i=0, j=1, nonOverlappingIntervals = 0;
-        
-        while(j<n) {
-            if(intervals[i][1] > intervals[j][0]) {
-                if(intervals[i][1] > intervals[j][1]) {
-                    i = j;
-                }
-                nonOverlappingIntervals++;
-            } else {
-                i = j;
-            }
-            j++;
-        }
-        
-        return nonOverlappingIntervals;
-    }
+  public int eraseOverlapIntervals(int[][] I) {
+      
+      int n = I.length;
+      if(n<2) return 0;
+      
+      int min = I[0][1];
+      int max = min;
+      int minStart = I[0][0];
+      for(int i=1; i<n; i++){
+          max = Math.max(max, I[i][1]);
+          min = Math.min(min, I[i][1]);
+          minStart = Math.min(minStart, I[i][0]);
+      }
+      
+      int shift = 1 - min;
+      int[] iMap = new int[max - min + 2];
+      for(int[] d:I){
+          int left = d[0]+shift;
+          int right = d[1]+shift;
+          if(left>iMap[right]){
+              iMap[right] = left;
+          }
+      }
+    
+      minStart = minStart + shift;      
+      int notOverlap = 0;
+      for(int i=1; i<iMap.length; i++){
+          if(iMap[i]>=minStart){
+              notOverlap++;
+              minStart = i;
+          }
+      }
+      
+      return n - notOverlap;
+  }
 }
