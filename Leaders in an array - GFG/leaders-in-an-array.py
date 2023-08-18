e0af5221d87088e4698887159d3ac0ab0fdabd38
Kaushal Kumar <90971842+kaushal32@users.noreply.github.com>
2023-08-18 22:22:40 +0530

    Added solution - LeetHub

---
+++ Leaders in an array - GFG/leaders-in-an-array.py
@@ -0,0 +1,40 @@
+from collections import deque
+class Solution:
+    def leaders(self, A, N):
+        res=deque([A[N-1]])
+        for i in range(N-2,-1,-1):
+            if res[0]<=A[i]:
+                res.appendleft(A[i])
+        return res
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        N=int(input())
+        
+        A=[int(x) for x in input().strip().split()]
+        obj = Solution()
+        
+        A=obj.leaders(A,N)
+        
+        for i in A:
+            print(i,end=" ")
+        print()
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
